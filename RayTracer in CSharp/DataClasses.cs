8dda5eadc670c8c8d5c2d61f96432be7c6770bf5
Marvin Müller <marvin.mueller@code.berlin>
2020-09-29 18:31:48 +0200

    Initial commit

---
+++ RayTracer in CSharp/DataClasses.cs
@@ -0,0 +1,359 @@
+﻿using System;
+
+namespace DataClasses
+{
+    public class Point3
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        #endregion
+
+        #region Functions
+        public Point3(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        #endregion
+
+        #region Conversion
+        #region Interclass-casting
+        public static implicit operator Color3(Point3 p)
+        {
+            return new Color3(p.X, p.Y, p.Z);
+        }
+
+        public static implicit operator Vector3(Point3 p)
+        {
+            return new Vector3(p.X, p.Y, p.Z);
+        }
+        #endregion
+        #endregion
+    }
+
+    public class Vector3
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Create a new 3-Dimensional Vector
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public Vector3(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        #endregion
+
+        #region Operators
+        /// <summary>
+        /// Just the itself vector with a plus in front of it should not change anything
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector3 operator +(Vector3 v) => v;
+
+        /// <summary>
+        /// Adds 2 Vectors together
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
+        public static Vector3 operator +(Vector3 vector1, Vector3 vector2)
+        {
+            vector1.X += vector2.X;
+            vector1.Y += vector2.Y;
+            vector1.Z += vector2.Z;
+            return vector1;
+        }
+
+        /// <summary>
+        /// A minus in front of the vector negates it
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Vector3 operator -(Vector3 v)
+        {
+            return new Vector3(-v.X, -v.Y, -v.Z);
+        }
+        /// <summary>
+        /// Subtracts two vectors from each other
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
+        public static Vector3 operator -(Vector3 vector1, Vector3 vector2)
+        {
+            vector1.X -= vector2.X;
+            vector1.Y -= vector2.Y;
+            vector1.Z -= vector2.Z;
+            return vector1;
+        }
+
+        /// <summary>
+        /// Multiplies all values of the vector
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="multiplier"></param>
+        /// <returns></returns>
+        public static Vector3 operator *(double multiplier, Vector3 vector)
+        {
+            return new Vector3(vector.X * multiplier, vector.Y * multiplier, vector.Z * multiplier);
+        }
+
+
+        /// <summary>
+        /// Multiplies all values of the vector
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="multiplier"></param>
+        /// <returns></returns>
+        public static Vector3 operator *(Vector3 vector, double multiplier)
+        {
+            return new Vector3(vector.X * multiplier, vector.Y * multiplier, vector.Z * multiplier);
+        }
+
+        /// <summary>
+        /// Multiplies 2 vectors with each other
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static Vector3 operator *(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z);
+        }
+
+        /// <summary>
+        /// Divides all values of the vector by a value
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="divident"></param>
+        /// <returns></returns>
+        public static Vector3 operator /(Vector3 vector, double divident)
+        {
+            return vector * (1 / divident);
+        }
+
+        /// <summary>
+        /// Makes the vector behave like a 3-item-long double-array
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public double this[int key]
+        {
+            get
+            {
+                return this.ToArray()[key];
+            }
+            set
+            {
+                double[] array = this.ToArray();
+                array[key] = value;
+                this.FromArray(array);
+            }
+        }
+        #endregion
+
+        #region Utility
+
+        /// <summary>
+        /// Gets the length of the vector from 0, 0, 0
+        /// </summary>
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(this.LengthSquared);
+            }
+        }
+
+        public double LengthSquared
+        {
+            get
+            {
+                return this.X * this.X + this.Y * this.Y + this.Z * this.Z;
+            }
+        }
+
+        /// <summary>
+        /// Calculate "unit vector" (Vector with the length of 1)
+        /// </summary>
+        public Vector3 UnitVector => (this / this.Length);
+
+        public double Dot(Vector3 targetVector)
+        {
+            return this.X * targetVector.X
+                 + this.Y * targetVector.Y
+                 + this.Z * targetVector.Z;
+        }
+
+        /// <summary>
+        /// Crosses two vectors
+        /// </summary>
+        /// <param name="targetVector"></param>
+        /// <returns></returns>
+        public Vector3 Cross(Vector3 targetVector)
+        {
+            return new Vector3(this.Y * targetVector.Z - this.Z * targetVector.Y,
+                               this.Z * targetVector.X - this.X * targetVector.Z,
+                               this.X * targetVector.Y - this.Y * targetVector.X);
+        }
+        #endregion
+
+        #region Conversion
+        /// <summary>
+        /// Converts this object's data and turns it into a double-array
+        /// </summary>
+        /// <returns></returns>
+        public double[] ToArray()
+        {
+            return new double[] { this.X, this.Y, this.Z };
+        }
+
+        /// <summary>
+        /// Takes data from a double-array and converts it to a Vector3 object
+        /// </summary>
+        /// <param name="array"></param>
+        public void FromArray(double[] array)
+        {
+            this.X = array[0];
+            this.Y = array[1];
+            this.Z = array[2];
+        }
+        #region Interclass-casting
+        public static implicit operator Color3(Vector3 v)
+        {
+            return new Color3(v.X, v.Y, v.Z);
+        }
+
+        public static implicit operator Point3(Vector3 v)
+        {
+            return new Point3(v.X, v.Y, v.Z);
+        }
+        #endregion
+        #endregion
+    }
+
+    public class Color3
+    {
+        #region Properties
+        public double R { get; set; }
+        public double G { get; set; }
+        public double B { get; set; }
+        #endregion
+
+        #region Functions
+        public Color3(double r, double g, double b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+        #endregion
+
+        #region Operators
+        public static Color3 operator *(double multiplier, Color3 color)
+        {
+            return new Color3(color.R * multiplier, color.G * multiplier, color.B * multiplier);
+        }
+
+        public static Color3 operator *(Color3 color, double multiplier)
+        {
+            return new Color3(color.R * multiplier, color.G * multiplier, color.B * multiplier);
+        }
+
+        public static Color3 operator +(Color3 color1, Color3 color2)
+        {
+            return new Color3(color1.R + color2.R, color1.G + color2.G, color1.B + color2.B);
+        }
+        #endregion
+
+        #region Conversion
+        public static implicit operator Vector3(Color3 c)
+        {
+            return new Vector3(c.R, c.G, c.B);
+        }
+
+        public static implicit operator Point3(Color3 c)
+        {
+            return new Point3(c.R, c.G, c.B);
+        }
+
+        public static implicit operator Color4(Color3 c)
+        {
+            return new Color4(c.R, c.G, c.B, 0);
+        }
+        #endregion
+    }
+
+    public class Color4
+    {
+        #region Properties
+        public double R { get; set; }
+        public double G { get; set; }
+        public double B { get; set; }
+        public double A { get; set; }
+        #endregion
+
+        #region Functions
+        public Color4(double r, double g, double b, double a)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+            this.A = a;
+        }
+        #endregion
+
+        #region Conversion
+        public static explicit operator Vector3(Color4 c)
+        {
+            return new Vector3(c.R, c.G, c.B);
+        }
+
+        public static explicit operator Point3(Color4 c)
+        {
+            return new Point3(c.R, c.G, c.B);
+        }
+
+        public static explicit operator Color3(Color4 c)
+        {
+            return new Color3(c.R, c.G, c.B);
+        }
+        #endregion
+    }
+
+    public class Ray
+    {
+        #region Properties
+        public Point3 Origin { get; set; }
+        public Vector3 Direction { get; set; }
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Creates a new ray
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="direction"></param>
+        public Ray(Point3 origin, Vector3 direction)
+        {
+            this.Origin = origin;
+            this.Direction = direction;
+        }
+        #endregion
+    }
+}
---
+++ RayTracer in CSharp/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using DataClasses;
+
+namespace RayTracer_in_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region Setup
+            // Create Camera to render
+            Camera camera = new Camera(400, 225); ///400x225 resolution
+
+            // Create the PPM file and open it
+            StreamWriter w = new StreamWriter(@".\uwu.ppm");
+
+            // PPM header
+            w.Write("P3\n" + camera.ResolutionWidth + " " + camera.ResolutionHeight + "\n255\n");
+
+            // camera setup
+            camera.ViewportHeight = 2.0;
+            camera.FocalLength = 1.0;
+            camera.Origin = new Point3(0, 0, 0);
+            #endregion
+
+            #region Render
+            // Render
+            for (int j = camera.ResolutionHeight - 1; j >= 0 ; --j)
+            {
+                // Announce progress
+                Console.Write("Scanlines remaining: {0}", j);
+                Console.CursorTop = 0;
+                Console.CursorLeft = 0;
+
+                for (int i = 0; i < camera.ResolutionWidth; ++i)
+                {
+                    double u = (double)i / (camera.ResolutionWidth - 1);
+                    double v = (double)j / (camera.ResolutionHeight - 1);
+                    Ray r = new Ray(camera.Origin, camera.LowerLeftCorner + u * camera.ViewportHorizontal + v * camera.ViewportVertical - camera.Origin);
+                    Color3 pixelColor = RayColor(r);
+                    pixelColor = pixelColor * 255;
+                    w.WriteLine(pixelColor.R + " " + pixelColor.G + " " + pixelColor.B);
+                }
+            }
+
+
+            #endregion
+
+            #region Cleanup
+            // Flush the stream to the drive and close the stream
+            w.Flush();
+            w.Close();
+            #endregion
+        }
+
+        static Color3 RayColor(Ray ray)
+        {
+            double t = 0.5 * (ray.Direction.UnitVector.Y + 1.0);
+            return (1.0 - t) * new Color3(1, 1, 1) + t * new Color3(0.5, 0.7, 1.0);
+
+        }
+
+#pragma warning disable IDE0051 // Remove unused private members
+        static void WriteColor(Color3 color)
+#pragma warning restore IDE0051 // Remove unused private members
+        {
+            Console.WriteLine(ColorToPpmLine(color));
+        }
+
+        static string ColorToPpmLine(Color3 color)
+        {
+            return (color.R * 255.999) + " " + (color.G * 255.999) + " " + (color.B * 255.999);
+        }
+    }
+
+    class Camera
+    {
+        #region Properties
+        #region Resolution
+        /// <summary>
+        /// The width of the image in pixels
+        /// </summary>
+        public int ResolutionWidth { get; set; }
+        /// <summary>
+        /// The height of the image in pixels
+        /// </summary>
+        public int ResolutionHeight { get; set; }
+        /// <summary>
+        /// resolutionX / resolutionY
+        /// </summary>
+        public double AspectRatio {
+            get
+            {
+                return this.ResolutionWidth / this.ResolutionHeight;
+            }
+        }
+        #endregion
+        #region Viewport and camera
+
+        /// <summary>
+        /// Height of the viewport (default: 2)
+        /// </summary>
+        public double ViewportHeight
+        {
+            get
+            {
+                return _ViewportHeight;
+            }
+            set
+            {
+                _ViewportHeight = value;
+                ViewportWidth = this.AspectRatio * _ViewportHeight;
+            }
+        }
+        public double ViewportWidth { get; private set; }
+        /// <summary>
+        /// Gets the horizontal vector of the viewport
+        /// </summary>
+        public Vector3 ViewportHorizontal
+        {
+            get
+            {
+                return new Vector3(ViewportWidth, 0, 0);
+            }
+        }
+        /// <summary>
+        /// /// Gets the vertical vector of the viewport
+        /// </summary>
+        public Vector3 ViewportVertical
+        {
+            get
+            {
+                return new Vector3(0, ViewportHeight, 0);
+            }
+        }
+        /// <summary>
+        /// Gets the lower left corner of the viewport as a vector
+        /// </summary>
+        public Vector3 LowerLeftCorner
+        {
+            get
+            {
+                return this.Origin - (ViewportHorizontal / 2) - (ViewportVertical / 2) - (new Vector3(0, 0, this.FocalLength));
+            }
+        }
+
+        /// <summary>
+        /// Sets the focal length (distance of viewport to camera-origin) (default: 1.0)
+        /// </summary>
+        public double FocalLength { get; set; } = 1.0;
+
+        /// <summary>
+        /// Sets the origin of the Camera (default: X = 0, Y = 0, Z = 0)
+        /// </summary>
+        public Point3 Origin { get; set; } = new Point3(0, 0, 0);
+
+        #endregion
+        #region Holder Variables
+        private double _ViewportHeight = 2;
+        #endregion
+        #endregion
+
+        #region Function
+        public Camera(int resolutionWidth, int resolutionHeight)
+        {
+            this.ResolutionWidth = resolutionWidth;
+            this.ResolutionHeight = resolutionHeight;
+        }
+        #endregion
+    }
+}
