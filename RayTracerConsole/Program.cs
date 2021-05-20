9d5a591103fec20ec4a474d43e4ffef3323f27e2
Marvin Müller <xdrone@hotmail.de>
2021-05-20 13:36:23 +0200

    Rewrite Console Renderer

--- RayTracerConsole/Program.cs
+++ RayTracerConsole/Program.cs
@@ -11,52 +11,74 @@ namespace RayTracerConsole
     {
         static void Main(string[] args)
         {
-            Camera camera = new Camera(1920, 1080)
+            bool writeDebugInfo = false;
+
+            Raytracer renderer = new Raytracer
             {
-                Origin = new Vector3(0, 0, 0),
+                // Define objects in the scene
+                HittableObjects = new HittableList
+                {
+                    new Sphere(0, 1, 0, 0.5),
+                    new Sphere(0.5, 1, 0, 0.5),
+                    new Sphere(0, 1, -100.5, 100)
+                }
+            };
+
+            // Define the camera
+            Camera camera = new Camera(720, 480)
+            {
+                Origin = new Vector3(-1, 0, 0),
                 MultithreadedRendering = true,
                 SamplesPerPixel = 100,
                 MaxBounces = 12
             };
-            Raytracer renderer = new Raytracer();
 
-            renderer.HittableObjects = new HittableList
-            {
-                new Sphere(0, 1, 0, 0.5),
-                new Sphere(0.5, 1, 0, 0.5),
-                new Sphere(0, 1, -100.5, 100)
-            };
-
-            const int frames = 10;
-            Task<RenderResult>[] tasks = new Task<RenderResult>[frames];
+            Console.WriteLine("Rendering...");
+            const int frames = 200;
+            long lastFrameTime = 0;
             for (int i = 0; i < frames; i++)
             {
+                // calculate time remaining
+                string timeRemaining = String.Empty;
+                if (lastFrameTime != 0)
+                {
+                    timeRemaining = "Time remaining: ~" + Math.Round(((double)lastFrameTime / 1000d / 60d) * frames).ToString() + " minutes";
+                }
+
+                // provide a status
+                Console.CursorLeft = 0;
+                Console.Write("{0} out of {1} completed. ({2} %) {3}", i, frames, Math.Round((float)(i) / ((float)frames / 100)), timeRemaining);
+
+                // define camera + changes to it
                 Camera localCam = new Camera(camera.ResolutionWidth, camera.ResolutionHeight)
                 {
                     Origin = camera.Origin + new Vector3(0.01f * i, 0, 0),
-                    MultithreadedRendering = false,
+                    MultithreadedRendering = camera.MultithreadedRendering,
                     SamplesPerPixel = camera.SamplesPerPixel,
-                    MaxBounces = 12
+                    MaxBounces = camera.MaxBounces
                 };
+
                 RenderResult result = new RenderResult()
                 {
                     frameNumber = i,
                     camera = localCam
                 };
 
-                tasks[i] = Task.Run(() =>
-                {
-                    result.pixels = renderer.RenderScene(localCam, out long frameTime);
-                    result.frameTime = frameTime;
-                    return result;
-                });
-            }
-            Task.WaitAll(tasks);
+                // Render
+                result.pixels = renderer.RenderScene(localCam, out long frameTime);
+                result.frameTime = frameTime;
 
-            for (int i = 0; i < frames; i++)
-            {
-                renderer.WriteFrame("./images/image_" + tasks[i].Result.frameNumber + ".png", tasks[i].Result.pixels, tasks[i].Result.camera.ResolutionHeight, tasks[i].Result.camera.ResolutionWidth, ImageFormat.Png, true, tasks[i].Result.frameTime, tasks[i].Result.camera);
+                // Write to disk
+                System.IO.Directory.CreateDirectory("./images/");
+                renderer.WriteFrame("./images/image_" + result.frameNumber + ".png", result.pixels, result.camera.ResolutionHeight, result.camera.ResolutionWidth, ImageFormat.Png, writeDebugInfo, result.frameTime, result.camera);
+
+                // record last frametime for time approximation
+                lastFrameTime = frameTime;
             }
+
+            Console.Clear();
+            Console.WriteLine("Done.");
+            Console.ReadKey();
         }
     }
 
