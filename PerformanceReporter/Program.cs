f2bfeebb4cf9f82957d5a9ed0d8d4ff6ae6ce5d8
Marvin Müller <marvin.mueller@code.berlin>
2021-05-26 13:15:37 +0200

    Add Performance Reporter Project

---
+++ PerformanceReporter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Raytracing;
+
+namespace PerformanceReporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raytracer raytracer = new Raytracer();
+
+            int xRes = 720;
+            int yRes = 480;
+
+            int runs = 10;
+
+            raytracer.HittableObjects = new HittableList()
+            {
+                new Sphere(0, 1, 0, 0.5),
+                new Sphere(0.5, 1, 0, 0.5),
+                new Sphere(0, 1, 0.6, 0.5),
+                new Sphere(0, 1, -100.5, 100)
+            };
+
+            // Define the camera
+            Camera camera = new Camera(xRes, yRes)
+            {
+                Origin = new Vector3(-1, 0, 0),
+                MultithreadedRendering = false,
+                SamplesPerPixel = 100,
+                MaxBounces = 12
+            };
+
+            for(int i = 0; i < runs; i++)
+            {
+                raytracer.RenderScene(camera, out long frameTime);
+
+                Console.WriteLine("Run {0} | Time: {1} | Time Per Pixel: {2}", i, frameTime, (double)frameTime / (double)(xRes * yRes));
+            }
+        }
+    }
+}
