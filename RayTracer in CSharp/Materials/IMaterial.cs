fe4a9ebdfede76c94b4be05f69e514e9d9a67cfe
Marvin Müller <marvin.mueller@code.berlin>
2021-05-28 11:30:29 +0200

    Update Material Interface to final implementation

--- RayTracer in CSharp/Materials/IMaterial.cs
+++ RayTracer in CSharp/Materials/IMaterial.cs
@@ -4,6 +4,14 @@ namespace Raytracing.Materials
 {
     public interface IMaterial
     {
-        public bool Scatter(Ray rayIn, HitRecord hitRecord, Color3 colorAttenuation, Ray scatteredRay);
+        /// <summary>
+        /// Determines if the light will be scattered or reflected
+        /// </summary>
+        /// <param name="rayIn">The previous ray</param>
+        /// <param name="hitRecord">Hitrecord of the previous raytrace</param>
+        /// <param name="colorAttenuation">Factor of light absorbed</param>
+        /// <param name="scatteredRay">The new directionn in which light will be scattered</param>
+        /// <returns>A scattered ray, if the light ends up scattering</returns>
+        public bool Scatter(Ray rayIn, HitRecord hitRecord, out Color3 colorAttenuation, out Ray scatteredRay);
     }
 }
