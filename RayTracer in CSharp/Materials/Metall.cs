b9ccfff8e977b6413aaa3a9cb9e21c36e837a0b8
Marvin Müller <marvin.mueller@code.berlin>
2021-05-29 08:24:02 +0200

    Add metal material

---
+++ RayTracer in CSharp/Materials/Metall.cs
@@ -0,0 +1,24 @@
+﻿using Raytracing.DataStructures;
+
+namespace Raytracing.Materials
+{
+    public class Metal : IMaterial
+    {
+        public Color3 Albedo { get; set; }
+
+        public Metal(Color3 albedo)
+        {
+            this.Albedo = albedo;
+        }
+
+        public bool Scatter(Ray rayIn, HitRecord hitRecord, out Color3 colorAttenuation, out Ray scatteredRay)
+        {
+            // reflect the ray off the surface and make a new ray originating from the hit-point
+            scatteredRay = new Ray(hitRecord.Point, Vector3.Reflect(rayIn.Direction.UnitVector, hitRecord.Normal));
+
+            colorAttenuation = this.Albedo;
+
+            return (Vector3.Dot(scatteredRay.Direction, hitRecord.Normal) > 0);
+        }
+    }
+}
