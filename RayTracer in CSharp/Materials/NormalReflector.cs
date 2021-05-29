ab435415d2cbf0d4c363bec7022dd55f5947455e
Marvin Müller <marvin.mueller@code.berlin>
2021-05-29 05:22:19 +0200

    Add NormalReflector Material

---
+++ RayTracer in CSharp/Materials/NormalReflector.cs
@@ -0,0 +1,21 @@
+﻿using Raytracing.DataStructures;
+
+namespace Raytracing.Materials
+{
+    public class NormalReflector : IMaterial
+    {
+        public Color3 Albedo;
+
+        public NormalReflector(Color3 albedo)
+        {
+            this.Albedo = albedo;
+        }
+
+        public bool Scatter(Ray rayIn, HitRecord hitRecord, out Color3 colorAttenuation, out Ray scatteredRay)
+        {
+            scatteredRay = new Ray(hitRecord.Point, hitRecord.Normal);
+            colorAttenuation = this.Albedo;
+            return true;
+        }
+    }
+}
