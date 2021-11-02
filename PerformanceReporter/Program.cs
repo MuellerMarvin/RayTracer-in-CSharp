fb9ba09480540816a1257b2195c5f6e127ca1098
Marvin Müller <marvin.mueller@code.berlin>
2021-11-02 17:48:45 +0100

    Fix up performance reporter

--- PerformanceReporter/Program.cs
+++ PerformanceReporter/Program.cs
@@ -10,8 +10,6 @@ namespace PerformanceReporter
     {
         static void Main(string[] args)
         {
-            Renderer raytracer = new Renderer();
-
             int xRes = 720;
             int yRes = 480;
 
@@ -23,7 +21,7 @@ namespace PerformanceReporter
             IMaterial red = new LambertianDiffuse(new Color3(0.8, 0.2, 0.2));
             IMaterial metal = new Metal(new Color3(0.9, 0.9, 0.9), 0);
 
-            Renderer renderer = new()
+            Renderer raytracer = new()
             {
                 // Define objects in the scene
                 HittableObjects = new HittableList
