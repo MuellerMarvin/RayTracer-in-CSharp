5e32a75128985a1a579fd079ab9d02a80c054415
Marvin Müller <marvin.mueller@code.berlin>
2021-11-04 13:54:01 +0100

    Add Resolution Pair

---
+++ RayTracer in CSharp/DataStructures/Resolution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raytracing.DataStructures
+{
+    public struct Resolution
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
